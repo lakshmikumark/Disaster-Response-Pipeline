5ecf8e88f34d58c7022d759c85bf0adca9b64196
Lakshmi Kumar Katikala <49107735+lakshmikumark@users.noreply.github.com>
2020-04-19 17:02:06 -0600

    Update process_data.py

--- data/process_data.py
+++ data/process_data.py
@@ -6,9 +6,12 @@ from sqlalchemy import create_engine
 
 def load_data(messages_filepath, categories_filepath):
 
-    messages = pd.read_csv(messages_filepath) # load messages dataset
-    categories = pd.read_csv(categaories_filepath) # load categories dataset
-    df = pd.merge(messages,categories, on='id') # merge datasets
+    # load messages, categories datasets
+    messages = pd.read_csv(messages_filepath)
+    categories = pd.read_csv(categaories_filepath)
+    
+    # merge the datasets
+    df = pd.merge(messages,categories, on='id')
     
     return df
 
@@ -22,18 +25,26 @@ def clean_data(df):
     a pandas dataframe after data clean
     """
     
-    categories = df.categories.str.split(pat=';', expand=True) # create a dataframe of the individual category columns
-    row = categories.iloc[0,:] # select the first row of the categories dataframe
-    category_colnames = row.apply(lambda x:x[:-2]) # use this row to extract a list of new column names for categories
-    categories.columns = category_colnames # rename the columns of `categories`
+    # create a dataframe of the individual category columns
+    categories = df.categories.str.split(pat=';', expand=True)
+    # select the first row of the categories dataframe
+    row = categories.iloc[0,:]
+    # use this row to extract a list of new column names for categories
+    category_colnames = row.apply(lambda x:x[:-2])
+    # rename the columns of `categories`
+    categories.columns = category_colnames
     
+    # set each value to be the last character of the string and convert this to numeric
     for column in categories:
-        categories[column] = categories[column].str[-1] # set each value to be the last character of the string
-        categories[column] = categories[column].astype(np.int) # convert column from string to numeric
-            
-    df = df.drop('categories' , axis=1) # drop the original categories column from `df`
-    df = pd.concat([df, categories],axis=1) # concatenate the original dataframe with the new `categories` dataframe
-    df = df.drop_duplicates() # drop duplicates 
+        categories[column] = categories[column].str[-1]
+        categories[column] = categories[column].astype(np.int)
+    
+    # drop the original categories column from `df` and concatenate with the new'categories' df
+    df = df.drop('categories' , axis=1) 
+    df = pd.concat([df, categories],axis=1)
+    
+    # drop duplicates
+    df = df.drop_duplicates()
     
     return df
 
@@ -45,8 +56,10 @@ def save_data(df, database_filename):
     output:
     save the dataframe into a sqllite database
     """
+    
+    # save the clean dataset into a sqlite database
     engine = create_engine('sqlite:///'+ database_filename)
-    df.to_sql('df', engine, index=False , if_exists='replace')  # save the clean dataset into a sqlite database
+    df.to_sql('df', engine, index=False , if_exists='replace')
     
 def main():
     if len(sys.argv) == 4:
